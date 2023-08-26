5af73fe63f2f15a4b03013da0ca309cfa5b77706
Ali <kly441781@gmail.com>
2023-08-26 22:15:44 +0330

    add delete view to code

--- final/urls.py
+++ final/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
     path('home/' , home,name='home'),
     path('create/user/final/' , Final_User_Create.as_view(),name='create'),
     path('update/user/final/<int:pk>' , Final_User_Update.as_view(),name='update'),
+    path('delete/user/final/<int:pk>' , Final_User_delete.as_view(),name='delete'),
     
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
